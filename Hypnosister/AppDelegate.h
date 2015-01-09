5994396f1e7ca538968dab1c557edb698767bfb2
Dustin Flanary <singflan@gmail.com>
2015-01-08 17:01:11 -0700

    Initial Commit

---
+++ Hypnosister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by Dustin Flanary on 1/8/15.
+//  Copyright (c) 2015 Dustin Flanary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hypnosister/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hypnosister
+//
+//  Created by Dustin Flanary on 1/8/15.
+//  Copyright (c) 2015 Dustin Flanary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
