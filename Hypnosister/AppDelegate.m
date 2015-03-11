42acfbb9a38285fc41bc5a4a4be3e5c64fdb845c
Dustin Flanary <singflan@gmail.com>
2015-03-10 19:32:48 -0600

    end of Chapter 5
    added random color changes to circles upon touch
    added a scroll view that scrolls between two views of same size that are the size of the screen.

--- Hypnosister/AppDelegate.m
+++ Hypnosister/AppDelegate.m
@@ -19,22 +19,35 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    //CGRect firstFrame = CGRectMake(160, 240, 100, 150);
-    CGRect firstFrame = self.window.bounds;
     
-    BNRHypnosisView *firstView = [[BNRHypnosisView alloc] initWithFrame:firstFrame];
-    //firstView.backgroundColor = [UIColor redColor];
+    //CGRect firstFrame = self.window.bounds;
+    //BNRHypnosisView *firstView = [[BNRHypnosisView alloc] initWithFrame:firstFrame];
+    //[self.window addSubview:firstView];
     
-    [self.window addSubview:firstView];
+    // Create CGRects for frames
+    CGRect screenRect = self.window.bounds;
+    CGRect bigRect = screenRect;
+    bigRect.size.width *= 2.0;
+    //bigRect.size.height *= 2.0;
     
-//    CGRect secondFrame = CGRectMake(20, 30, 50, 50);ww44
-//    
-//    BNRHypnosisView *secondView = [[BNRHypnosisView alloc] initWithFrame:secondFrame];
-//    secondView.backgroundColor = [UIColor blueColor];
-//    
-//    //[self.window addSubview:secondView];
-//    [firstView addSubview:secondView];
+    // Create a screen-sized scroll view and add it to the window
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:screenRect];
+    scrollView.pagingEnabled = YES;
+    [self.window addSubview:scrollView];
     
+//    // Create a super-sized hypnosis view and add it to the scroll view
+//    BNRHypnosisView *hypnosisView = [[BNRHypnosisView alloc] initWithFrame:bigRect];
+    // Create a screen-sized hypnosis view and add it to the scroll view
+    BNRHypnosisView *hypnosisView = [[BNRHypnosisView alloc] initWithFrame:screenRect];
+    [scrollView addSubview:hypnosisView];
+    
+    // Add a second screen-sized hypnosis view just off screen to the right
+    screenRect.origin.x += screenRect.size.width;
+    BNRHypnosisView *anotherView = [[BNRHypnosisView alloc] initWithFrame:screenRect];
+    [scrollView addSubview:anotherView];
+    
+    // Tell the scroll view how big its content area is
+    scrollView.contentSize = bigRect.size;
     
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
--- Hypnosister/BNRHypnosisView.m
+++ Hypnosister/BNRHypnosisView.m
@@ -8,6 +8,11 @@
 
 #import "BNRHypnosisView.h"
 
+@interface BNRHypnosisView ()
+
+@property (strong, nonatomic) UIColor *circleColor;
+
+@end
 
 @implementation BNRHypnosisView
 
@@ -17,6 +22,7 @@
     if (self) {
         //All BNRHypnosisViews start with a clear background color
         self.backgroundColor = [UIColor clearColor];
+        self.circleColor = [UIColor lightGrayColor];
     }
     return self;
 }
@@ -61,14 +67,37 @@
     path.lineWidth = 10;
     
     //Configure the drawing color to light gray
-    [[UIColor lightGrayColor] setStroke];
-     
+    //[[UIColor lightGrayColor] setStroke];
+    [self.circleColor setStroke];
+    
     //Draw the line!
     [path stroke];
     
 }
 
+//When a finger touches the screen
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    NSLog(@"%@ was touched", self);
+    
+    //Get 3 random numbers between 0 and 1
+    float red = (arc4random() % 100) / 100.0;
+    float green = (arc4random() % 100) / 100.0;
+    float blue = (arc4random() % 100) / 100.0;
+    
+    UIColor *randomColor = [UIColor colorWithRed:red
+                                           green:green
+                                            blue:blue
+                                           alpha:1.0];
+    
+    self.circleColor = randomColor;
+}
 
+- (void)setCircleColor:(UIColor *)circleColor
+{
+    _circleColor = circleColor;
+    [self setNeedsDisplay];
+}
 
 
 
